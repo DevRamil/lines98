2d8250177de7437845a8734bac3561e27d38dba7
DevRamil <dev.ramil.khayr@gmail.com>
2020-03-12 08:45:04 +0300

    #2
    Continue testing.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from tkinter import Label
+from tkinter import *
 
 
-widget = Label(None, text='Hello my world!')
-widget.pack()
-widget.mainloop()
+root = Tk()
+
+widget = Label(root)
+widget.config(text='Hello my world!')
+widget.pack(side=TOP, expand=YES, fill=BOTH)
+
+root.title('lines98')
+root.mainloop()
