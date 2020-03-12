63a55a567c308a6a92a0ddbac3f14ba0e9a56537
DevRamil <dev.ramil.khayr@gmail.com>
2020-03-12 09:29:19 +0300

    #3
    Test button.

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from tkinter import *
 
 root = Tk()
 
+w_button_1 = Button(root, text='Файл')
+w_button_1.pack(side=LEFT)
+
+
 widget = Label(root)
 widget.config(text='Hello my world!')
 widget.pack(side=TOP, expand=YES, fill=BOTH)
